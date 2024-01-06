73d86390ee09e5fe5501569e504b64069f5c243d
Vince <vinks59@gmail.com>
2024-01-05 19:34:16 -0600

    update redirect to use prod environment vars

--- functions/orgchart.ts
+++ functions/orgchart.ts
@@ -1,4 +1,8 @@
-export const onRequestGet: PagesFunction<any> = async (context) => {
-    const indexUrl = 'http://localhost:8788';
+interface Env {
+	CF_PAGES_URL: string;
+}
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+    const indexUrl = context.env.CF_PAGES_URL
  	return Response.redirect(indexUrl);
 }
